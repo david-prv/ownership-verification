978522c6d362603cdd9ecce0ce221eff1f848fd9
David <66866223+david-prv@users.noreply.github.com>
2023-11-21 23:12:56 +0100

    simpl

--- app.py
+++ app.py
@@ -14,13 +14,15 @@ def main() -> Any:
     _check = len(sys.argv) > 2 and str(sys.argv[2]).lower() == "verify"
 
     try:
+        _candidate = Candidate(_url)
+
         # check if the given url contains the verification
         # snippet and verify the placed token to be valid
         if _check:
             print("checking... ", end="", flush=True)
 
             # verifier is just an html parser
-            verifier = Checker(Candidate(_url))
+            verifier = Checker(_candidate)
             verifier.feed(requests.get(_url).text)
 
             # append result to previous line
@@ -29,10 +31,7 @@ def main() -> Any:
         
         # generate a new verification html snipped
         # that has to be placed on the webpage to verify
-        candidate = Candidate(_url)
-
-        # print multiline html string
-        print(candidate.get_verification_html())
+        print(_candidate.get_verification_html())
     except (ValueError, AssertionError) as err:
         print(BAD_RESULT)
         print(f"\nerror: {err}")
